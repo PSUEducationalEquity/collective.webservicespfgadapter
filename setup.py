4dcb7555ed8645ab815dce508c5d0881fd23483f
Paul Rentschler <paul@rentschler.ws>
2015-07-21 14:51:48 -0400

    base product

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='collective.webservicespfgadapter',
+      version=version,
+      description="PloneFormGen adapter that submits the form values \
+          to a web service API",
+      long_description=open("README.txt").read() + "\n" +
+                       open("CHANGES.txt").read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='Zope CMF Plone Web Service API PloneFormGen forms integration',
+      author='Paul Rentschler',
+      author_email='par117@psu.edu',
+      url='',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'Products.PloneFormGen>=1.7.0',
+          'Products.TALESField',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
+
